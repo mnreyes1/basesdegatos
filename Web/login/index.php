0a35c5ea848952de6c94cdfde427acb98c1fb110
matias <mnreyes1@uc.cl>
2019-10-06 13:10:26 -0300

    Added toy login index

---
+++ Web/login/index.php
@@ -0,0 +1,16 @@
+<link href="../styles/login.css" rel="stylesheet">
+<div style="text-align: center;">
+<h1>¿Como desea ingresar?</h1>
+<br>
+<body>
+    <div style="text-align: center;">
+            <form align="center" action="login_socio.php" method="post">
+                <button type="submit">Ingresar como socio</button>
+            </form>
+            <br>
+            <form align="center" action="login_ong.php" method="post">
+                <button type="submit">Ingresar como ONG</button>
+            </form>
+    </div>
+</body>
+</div>
---
+++ Web/login/login_socio.php
@@ -0,0 +1,37 @@
+<link href="../styles/login.css" rel="stylesheet">
+<!DOCTYPE php>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+<body>
+
+<div style="text-align: center;">
+    <h2>Ingresar como socio</h2>
+    
+    <form action="/action_page.php" method="post">
+    
+    <div class="container">
+      <label for="uname"><b>Nombre y apellido socio</b></label>
+      <br>
+      <input type="text" placeholder="Ingrese nombre y apellido de socio" name="uname" required autofocus=true>
+      <br>
+      <br>
+      
+      <label for="psw"><b>Contraseña</b></label>
+      <br>
+      <input type="password" placeholder="Ingrese contraseña" name="psw" required>
+      <br>
+      
+      <button type="submit">Ingresar</button>
+      <br>
+    </div>
+    
+    <div class="container" style="background-color:#f1f1f1">
+        <button type="button" class="cancelbtn">Volver</button>
+      </div>
+    </form>
+    
+  </div>
+  </body>
+  </html>
