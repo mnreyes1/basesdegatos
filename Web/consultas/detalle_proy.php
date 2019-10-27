f9725257cf8f46b93aa791f207ed8b890db7be4f
nep0s <48266811+nep0s@users.noreply.github.com>
2019-10-27 16:25:16 -0300

    detalles proyectos

--- Web/consultas/detalle_proy.php
+++ Web/consultas/detalle_proy.php
@@ -12,30 +12,37 @@
   #Llama a conexión, crea el objeto PDO y obtiene la variable $db
   require("../config/conexion.php");
 
- 	$query = "SELECT nombre, pais, descripcion FROM ONG
-	WHERE nombre = '".$nombre."';";
-	$result = $db2 -> prepare($query);
+ 	$query = "SELECT pnombre, tipo, latitud, longitud, comuna, fecha_apertura,
+  operativo FROM Proyectos
+	WHERE pnombre = '".$nombre."';";
+	$result = $db1 -> prepare($query);
 	$result -> execute();
 	$datos = $result -> fetchAll();
   ?>
 
     <table cellspacing=5>
         <tr>
-            <th>Nombre ONG</th>
-            <th>País</th>
-            <th>Descripción</th>
-            <th>Movilizaciones</th>
-            <th>Recursos</th>
+            <th>Nombre Proyecto</th>
+            <th>Tipo</th>
+            <th>Latitud</th>
+            <th>Longitud</th>
+            <th>Comuna</th>
+            <th>Fecha de apertura</th>
+            <th>Operativo</th>
+            <th>Recursos de protección</th>
         </tr>
         <?php
 	foreach ($datos as $data) {
   		echo "<tr>
-			   <td>$data[0]</td>
-                           <td>$data[1]</td>
-                           <td>$data[2]</td>
-                           <td><a href=\"detalle_mov.php?id=$data[0]\">Movilizaciones</a></td>
-                           <td><a href=\"detalle_rec_open.php?id=$data[0]\">Recursos</a></td>
-                </tr>";
+              <td align=center>$data[0]</td>
+              <td align=center>$data[1]</td>
+              <td align=center>$data[2]</td>
+              <td align=center>$data[3]</td>
+              <td align=center>$data[4]</td>
+              <td align=center>$data[5]</td>
+              <td align=center>$data[6]</td>
+              <td align=center><a href=\"detalle_rec_open.php?id=$data[0]\">Recursos</a></td>
+            </tr>";
 	}
   ?>
     </table>
